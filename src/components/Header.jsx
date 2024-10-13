738b815062772244f544cc716e6831f0ad1f0f10
WojciechZ04 <wojtekz.6776@gmail.com>
2024-10-13 10:55:13 +0200

    add Header and UserInput components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header id="header">
+      <img
+        src="src\assets\investment-calculator-logo.png"
+        alt="Investment calculator logo"
+      />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
